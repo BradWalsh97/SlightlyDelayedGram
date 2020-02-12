0bde81d938b690be8a5dd843aa5231c57b20c373
TheLilGrant <60147677+TheLilGrant@users.noreply.github.com>
2020-02-12 17:35:31 -0500

    Applied Nick's HTML / CSS Templates to the Site Main Page

--- SlightlyDelayedGram/users/views.py
+++ SlightlyDelayedGram/users/views.py
@@ -6,6 +6,34 @@ from django.utils import timezone
 from .forms import UserRegisterForm
 from .models import Picture
 
+posts = [
+    {
+        'author': 'Jonathan',
+        'title': 'Test Post 1',
+        'content': 'Check out this sick pic',
+        'date_posted': 'January 17, 2019'
+    },
+    {
+        'author': 'Jonathan',
+        'title': 'Test Post 2',
+        'content': 'Check out this sick pic',
+        'date_posted': 'January 18, 2019'
+    },
+    {
+        'author': 'Jonathan',
+        'title': 'Test Post 3',
+        'content': 'Check out this sick pic',
+        'date_posted': 'January 19, 2019'
+    },
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'users/main.html', context)
+
 
 def register(request):
     if request.method == 'POST':
@@ -25,6 +53,7 @@ def profile(request):
     latest_picture_list = Picture.objects.filter(owner=request.user).order_by('-post_date')
     context = {'latest_picture_list': latest_picture_list}
     return render(request,'users/profile.html', context)
+
 
 def upload_picture(request):
     try:
