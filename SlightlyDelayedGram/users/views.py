34717956cd9f75c451764701fb927caa7cb6ff51
NihlathakTheNecromancer <chrisgalante22@gmail.com>
2020-02-11 19:42:44 -0500

    Created model for uploading pictures

--- SlightlyDelayedGram/users/views.py
+++ SlightlyDelayedGram/users/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from django.http import HttpResponse, HttpResponseForbidden
+from django.utils import timezone
+from .forms import UserRegisterForm, PictureUploadForm
+from .models import Picture
 
 def register(request):
     if request.method == 'POST':
@@ -17,4 +20,19 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
+    latest_picture_list = Picture.objects.filter(owner=request.user).order_by('-post_date')[:5]
+    context = {'latest_picture_list': latest_picture_list}
+    return render(request,'users/profile.html', context)
+
+def upload_picture(request): 
+    if request.method == 'POST': 
+        form = PictureUploadForm(request.POST, request.FILES)        
+        if form.is_valid(): 
+            form.save() 
+            return redirect('success') 
+    else: 
+        form = PictureUploadForm()
+    return render(request, 'profile.html', {'form' : form}) 
+   
+def success(request): 
+    return HttpResponse('successfully uploaded')
